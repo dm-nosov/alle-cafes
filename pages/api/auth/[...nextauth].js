37fa95084147c2ce1f232c082ef6f4ae1eac79cb
Dmitry Nosov <nosov.sibers@gmail.com>
2023-08-08 14:52:14 +0200

    Rename the NEXT_PUBLIC_SECRET variable

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
-  secret: process.env.SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
