ce4eb37016d845a9acce5ad78accec97fbc05956
Dmitry Nosov <nosov.sibers@gmail.com>
2023-08-08 14:44:55 +0200

    Add secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
