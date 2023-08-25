7efce7080f2772aff1668ef1d0f77fa402b39869
Dmitry Nosov <nosov.sibers@gmail.com>
2023-08-25 13:04:50 +0200

    Hotfix: Update the text contrast

--- components/OriginalPrice/index.js
+++ components/OriginalPrice/index.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Price } from "../Price";
 export const OriginalPrice = styled(Price)`
   text-decoration: line-through;
-  color: var(--editor-background);
+  color: black;
   line-height: 0.9rem;
   font-size: 0.7rem;
 `;
